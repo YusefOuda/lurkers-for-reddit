2cef1250193a5f27ea628b953bde6957353884d8
Yusef Ouda <yiouda92@gmail.com>
2019-05-15 22:33:07 -0500

    adds clicking links in comments and clicking headers to open links

--- lib/comment_view.dart
+++ lib/comment_view.dart
@@ -3,6 +3,7 @@ import 'package:draw/draw.dart' as Dart;
 import 'package:lurkers_for_reddit/time_converter.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:html_unescape/html_unescape.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class _CommentViewState extends State<CommentView> {
   @override
@@ -24,18 +25,29 @@ class _CommentViewState extends State<CommentView> {
           padding: EdgeInsets.only(left: 8.0),
           child: Column(
             children: <Widget>[
-              Row(children: <Widget>[
-                Text("/u/${widget.comment.author}", style: Theme.of(context).textTheme.subtitle,),
-                Text("  • "),
-                Text("${widget.comment.score} points"),
-                Text("  • "),
-                Text("${TimeConverter.convertUtcToDiffString(widget.comment.createdUtc)}")
-              ],),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "/u/${widget.comment.author}",
+                    style: Theme.of(context).textTheme.subtitle,
+                  ),
+                  Text("  • "),
+                  Text("${widget.comment.score} points"),
+                  Text("  • "),
+                  Text(
+                      "${TimeConverter.convertUtcToDiffString(widget.comment.createdUtc)}")
+                ],
+              ),
               Row(
                 children: <Widget>[
                   Flexible(
-                    child: MarkdownBody(data: unescape.convert(widget.comment.body)),// Text(widget.comment.body),
-                  ),
+                    child: MarkdownBody(
+                      data: unescape.convert(widget.comment.body),
+                      onTapLink: (url) {
+                        _handleLink(url);
+                      },
+                    ),
+                  ), // Text(widget.comment.body),
                 ],
               ),
             ],
@@ -43,6 +55,14 @@ class _CommentViewState extends State<CommentView> {
         ),
       ),
     );
+  }
+
+  _handleLink(url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw "cant open link";
+    }
   }
 }
 
--- lib/submission_body.dart
+++ lib/submission_body.dart
@@ -1,29 +1,35 @@
 import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart' as material;
+import 'package:url_launcher/url_launcher.dart';
 
 class SubmissionBodyState extends State<SubmissionBody> {
   String getImageUrl() {
-      String imageUrl = "";
-      if (widget.submission.isSelf) return imageUrl;
+    String imageUrl = "";
+    if (widget.submission.isSelf) return imageUrl;
 
-      if (widget.submission.variants.length > 0) {
-        var gifVariant = widget.submission.variants.firstWhere((x) => x.containsKey('gif')).values.toList()[0];
-        if (gifVariant != null) {
-          imageUrl = gifVariant.source.url.toString();
-        }
-      } else if (widget.submission.preview.length > 0) {
-        imageUrl = widget.submission.preview[0].source.url.toString();
-      } else {
-        imageUrl = widget.submission.url.toString();
+    if (widget.submission.variants.length > 0) {
+      var gifVariant = widget.submission.variants
+          .firstWhere((x) => x.containsKey('gif'))
+          .values
+          .toList()[0];
+      if (gifVariant != null) {
+        imageUrl = gifVariant.source.url.toString();
       }
+    } else if (widget.submission.preview.length > 0) {
+      imageUrl = widget.submission.preview[0].source.url.toString();
+    } else {
+      imageUrl = widget.submission.url.toString();
+    }
 
-      imageUrl = imageUrl.replaceAll('amp;', '');
-      return imageUrl;
+    imageUrl = imageUrl.replaceAll('amp;', '');
+    return imageUrl;
   }
+
   @override
   Widget build(BuildContext context) {
     var url = getImageUrl();
+    String postUrl = widget.submission.url.toString();
     print(url);
     return SliverAppBar(
       expandedHeight: MediaQuery.of(context).size.height - 400,
@@ -31,13 +37,24 @@ class SubmissionBodyState extends State<SubmissionBody> {
         collapseMode: CollapseMode.parallax,
         background: material.Visibility(
           visible: url.isNotEmpty,
-          child: Image.network(
-              url,
-              fit: BoxFit.cover),
+          child: InkWell(
+            onTap: () {
+              _handleLink(postUrl);
+            },
+            child: Image.network(url, fit: BoxFit.cover),
+          ),
           replacement: Container(),
         ),
       ),
     );
+  }
+
+  _handleLink(url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw "cant open link";
+    }
   }
 }
 
