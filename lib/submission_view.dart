41ffc3537a940178d252d6a9f69aa98965553fac
Yusef Ouda <yiouda92@gmail.com>
2019-05-16 22:07:00 -0500

    upvotes/downvotes

--- lib/submission_view.dart
+++ lib/submission_view.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:draw/draw.dart' as Dart;
 import 'package:lurkers_for_reddit/submission_comments.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:lurkers_for_reddit/time_converter.dart';
 
 class _SubmissionViewState extends State<SubmissionView> {
+  Color upvoteColor = Colors.white;
+  Color downvoteColor = Colors.white;
   @override
   Widget build(BuildContext context) {
     var thumb = widget.submission.thumbnail;
-    bool showThumbnail =
-        thumb != null && thumb.scheme.startsWith('http');
+    bool showThumbnail = thumb != null && thumb.scheme.startsWith('http');
     return IntrinsicHeight(
       child: InkWell(
         onTap: () {
@@ -67,11 +69,29 @@ class _SubmissionViewState extends State<SubmissionView> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          Text(
-                            "${widget.submission.subreddit.path}",
-                            style: Theme.of(context).textTheme.caption,
+                          Expanded(
+                            flex: 2,
+                            child: Text(
+                              "${widget.submission.subreddit.path}",
+                              style: Theme.of(context).textTheme.caption,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ),
-                          Text("${widget.submission.numComments} comments")
+                          Expanded(
+                            child: Text(
+                              TimeConverter.convertUtcToDiffString(
+                                  widget.submission.createdUtc),
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              "${widget.submission.numComments} cmts",
+                              maxLines: 1,
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                          ),
                         ],
                       ),
                     ],
@@ -87,22 +107,39 @@ class _SubmissionViewState extends State<SubmissionView> {
                           iconSize: 3.0,
                           icon: Icon(
                             Icons.arrow_upward,
-                            color: Colors.black,
+                            color: upvoteColor,
                             size: 24.0,
                           ),
                           onPressed: () {
-                            print("UPVOTED");
+                            setState(() {
+                              upvoteColor = Colors.orange;
+                              downvoteColor = Colors.white;
+                            });
+                            widget.submission.downvote().then((x) {
+                              print("upvoted");
+                            });
                           },
+                        ),
+                        Text(
+                          "${widget.submission.score}",
+                          maxLines: 1,
+                          style: Theme.of(context).textTheme.caption.copyWith(color: widget.submission.score > 0 ? Colors.orange : Colors.blue),
                         ),
                         IconButton(
                           iconSize: 3.0,
                           icon: Icon(
                             Icons.arrow_downward,
-                            color: Colors.black,
+                            color: downvoteColor,
                             size: 24.0,
                           ),
                           onPressed: () {
-                            print("DOWNVOTED");
+                            setState(() {
+                              upvoteColor = Colors.white;
+                              downvoteColor = Colors.blue;
+                            });
+                            widget.submission.downvote().then((x) {
+                              print("downvoted");
+                            });
                           },
                         ),
                       ],
